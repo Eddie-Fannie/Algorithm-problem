8379e5bcf21f23f8c7b9a0c86ec6dd5bc4191f4f
Eddie-Fannie <1597271709@qq.com>
2020-04-09 13:08:55 +0800

    add other methods

--- algorithm-demo/两数之和.js
+++ algorithm-demo/两数之和.js
@@ -23,6 +23,7 @@ var twoSum = function(nums, target) {
 twoSum([2, 7, 11, 15],9)
 
 //大神解答
+//1. 利用map
 var twoSum = function(nums, target) {
     let map = new Map()
     for(let i = 0; i< nums.length; i++) {
@@ -34,3 +35,37 @@ var twoSum = function(nums, target) {
     }
     return [];
 };
+
+//2.暴力解法
+/**
+* @param {number[]} nums
+* @param {number} target
+* @return {number[]}
+*/
+var twoSum = function(nums, target) {
+    for(var i = 0; i<nums.length; i++) {
+      var dif = target - nums[i];
+      // j = i+1 的目的是减少重复计算和避免两个元素下标相同
+      for(var j = i+1;j<nums.length;j++) {
+        if(nums[j] == dif)
+          return [i,j]
+      }
+    }
+  }
+
+//3.数组减少查询时间
+/**
+* @param {number[]} nums
+* @param {number} target
+* @return {number[]}
+*/
+var twoSum = function(nums, target) {
+    var temp = []
+    for(var i = 0;i<nums.length;i++) {
+      var dir = target - nums[i];
+      if(temp[dif] != undefined){
+        return [temp[dir], i];
+      }
+      temp[nums[i]] = i;
+    }
+}
